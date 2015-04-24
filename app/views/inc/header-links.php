7c7c448be4cf4cd316b1bb90a7f3b56d0136a56f
Ivo Tanev <ifch0o69@gmail.com>
2015-04-24 22:48:02 +0300

    header links updated.

--- app/views/inc/header-links.php
+++ app/views/inc/header-links.php
@@ -1,8 +1,8 @@
 <div id="left_side_header">
     <a href="/"><h1 id="app_name">Linked House</h1></a>
     <ul id="header_links_list">
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Facebook</a></li>
+        <li><a href="https://linkedhouse.wordpress.com/">Blog</a></li>
+        <li><a href="https://www.facebook.com/pages/Linked-House/724567267583137">Facebook</a></li>
         <li><a href="/about">About</a></li>
     </ul>
 </div>
